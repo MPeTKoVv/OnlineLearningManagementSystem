c01b3c42dfef334845714df5cd1a0b03c267b1d9
M.PeTKoVv <petkov.martin19@gmail.com>
2024-12-12 19:49:39 +0200

    implement login

--- LearningSystem.ViewModels/User/RegisterFormModel.cs
+++ LearningSystem.ViewModels/User/RegisterFormModel.cs
@@ -25,10 +25,12 @@
 
         [Required]
         [StringLength(FirstNameMaxLength, MinimumLength = FirstNameMinLength)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; } = null!;
 
         [Required]
         [StringLength(LastNameMaxLength, MinimumLength = LastNameMinLength)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; } = null!;
     }
 }
